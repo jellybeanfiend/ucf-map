62bdd5b63a11550746d0e1906261dda4afbf2852
Sevske <tookieblade@gmail.com>
2014-09-21 22:25:41 -0400

    First commit - basic android application with map

---
+++ src/com/sevenaskeels/ucfmap/MainActivity.java
@@ -0,0 +1,48 @@
+package com.sevenaskeels.ucfmap;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.model.*;
+import android.app.Activity;
+
+
+public class MainActivity extends Activity {
+	
+	private GoogleMap map;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map))
+                .getMap();
+        
+        LatLngBounds campus = new LatLngBounds(
+        		  new LatLng(27, -81), new LatLng(28, -82));
+        LatLng center = new LatLng(28.602, -81.200);
+
+        		map.moveCamera(CameraUpdateFactory.newLatLngZoom(center, 17));
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
